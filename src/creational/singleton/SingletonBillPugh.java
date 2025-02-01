d703a801593649dd67d73e152700fd67f1740925
jeffersontdm <jeffersontavares1611@gmail.com>
2025-02-01 09:15:47 -0300

    feat: singleton pattern

---
+++ src/creational/singleton/SingletonBillPugh.java
@@ -0,0 +1,18 @@
+package creational.singleton;
+
+/**
+ * Bill Pugh Singleton Pattern
+ */
+public class SingletonBillPugh {
+
+  private SingletonBillPugh() { }
+
+  private static final class InstanceHolder {
+    private static final SingletonBillPugh instance = new SingletonBillPugh();
+  }
+
+  public static SingletonBillPugh getInstance() {
+    return InstanceHolder.instance;
+  }
+}
+
---
+++ src/creational/singleton/SingletonDoubleCheckedLocking.java
@@ -0,0 +1,22 @@
+package creational.singleton;
+
+/**
+ * Double-Checked Locking Singleton Pattern
+ */
+public class SingletonDoubleCheckedLocking {
+  private static volatile SingletonDoubleCheckedLocking instance;
+
+  private SingletonDoubleCheckedLocking() { }
+
+  public static SingletonDoubleCheckedLocking getInstance() {
+    if (instance == null) {
+      synchronized (SingletonDoubleCheckedLocking.class) {
+        if (instance == null) {
+          instance = new SingletonDoubleCheckedLocking();
+        }
+      }
+    }
+
+    return instance;
+  }
+}
